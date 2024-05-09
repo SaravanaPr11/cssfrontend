193a9ff3c77f314c7aef265ba843d249efcb1a7a
SaravanaPr11 <167080065+SaravanaPr11@users.noreply.github.com>
2024-05-09 08:26:07 +0530

    added requests

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import AccountStatement from './Components/AccountStatement';
 import ServiceRequestMenu from "./Components/ServiceRequestMenu";
 import CustomerServiceMenu from "./Components/CustomerServiceMenu";
 import RequestStatus from './Components/viewrequest';
+import ChequeBookRequest from './Components/ChequeBookRequest';
+import StolenDebitCard from './Components/StolenDebitCard';
+import CreditCardRequest from './Components/CreditCardRequest';
 import Profile from './Components/profile';
 import Header from './Components/Header';
 import React from 'react';
@@ -20,10 +23,13 @@ function App() {
        <Routes>
           <Route exact path='/' element={<LoginForm />} />
           <Route exact path='/CustomerServiceMenu' element={<CustomerServiceMenu />} />
-          <Route exact path='/profile' element={<Profile />}></Route>
           <Route exact path='/MyAccount' element={<MyAccount />} />
           <Route exact path='/AccountStatement' element={<AccountStatement />} />
           <Route exact path='/ServiceRequestMenu' element={<ServiceRequestMenu />} />
+          <Route exact path='/ChequeBookRequest' element={<ChequeBookRequest />} />
+          <Route exact path='/StolenDebitCard' element={<StolenDebitCard />} />
+          <Route exact path='/CreditCardRequest' element={<CreditCardRequest />} />
+          <Route exact path='/profile' element={<Profile />}></Route>
           <Route exact path='/view' element={<RequestStatus/>}></Route>
         </Routes>
         <Footer />
