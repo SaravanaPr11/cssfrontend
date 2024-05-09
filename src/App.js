e5e7521b6b6695efcc9b707eeda98d1ecadb7df6
SaravanaPr11 <167080065+SaravanaPr11@users.noreply.github.com>
2024-05-09 07:17:35 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import Footer from "./Components/Footer";
 import LoginForm from './Components/loginform';
-import MyAccount from "./Components/MyAccount"; 
+import CustomerServiceMenu from "./Components/CustomerServiceMenu";
+import Profile from './Components/profile';
+import MyAccount from "./Components/MyAccount";
 import AccountStatement from './Components/AccountStatement';
 import ServiceRequestMenu from "./Components/ServiceRequestMenu";
-import CustomerServiceMenu from "./Components/CustomerServiceMenu";
+import ChequeBookRequest from './Components/ChequeBookRequest';
+import StolenDebitCard from './Components/StolenDebitCard';
+import CreditCardRequest from './Components/CreditCardRequest';
 import RequestStatus from './Components/viewrequest';
-import Profile from './Components/profile';
 import Header from './Components/Header';
 import React from 'react';
 import './App.css';
@@ -24,6 +27,9 @@ function App() {
           <Route exact path='/MyAccount' element={<MyAccount />} />
           <Route exact path='/AccountStatement' element={<AccountStatement />} />
           <Route exact path='/ServiceRequestMenu' element={<ServiceRequestMenu />} />
+          <Route exact path='/ChequeBookRequest' element={<ChequeBookRequest />} />
+          <Route exact path='/StolenDebitCard' element={<StolenDebitCard />} />
+          <Route exact path='/CreditCardRequest' element={<CreditCardRequest />} />
           <Route exact path='/view' element={<RequestStatus/>}></Route>
         </Routes>
         <Footer />
