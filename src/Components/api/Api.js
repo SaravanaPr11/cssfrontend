60c6e71a44b521ada3b15a92a28ad5146a89e99a
SaravanaPr11 <167080065+SaravanaPr11@users.noreply.github.com>
2024-06-21 14:27:06 +0530

    update

--- src/Components/api/Api.js
+++ src/Components/api/Api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useAuth } from './AuthContext';
+import { useAuth } from '../authContext';
 
 const api = axios.create({
   baseURL: 'http://localhost:2026/bankapps',
